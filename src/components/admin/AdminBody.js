96c147036a4615f7f0f812f7e3fa233883078f93
Kristoffer Kyle Fernandez <kfernz2626@gmail.com>
2022-07-17 18:44:04 +0800

    Added Tabs to admin Body

--- src/components/admin/AdminBody.js
+++ src/components/admin/AdminBody.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
+import { Box, Tabs, Tab } from "@mui/material";
 import AdminTable from "./AdminTable";
 import AdminCreateForm from "./AdminCreateForm";
 import AdminUpdateForm from "./AdminUpdateForm";
+import TabPanel from "../global/TabPanel";
 import apiClient from "../../services/api";
 
 export default function AdminBody() {
 	const [users, setUsers] = useState([]);
 	const [nextPageUrl, setNextPageUrl] = useState("");
+	const [tabValue, setTabValue] = useState(0);
 	const token = JSON.parse(localStorage.getItem("user")).token;
 	const headers = {
 		accept: "application/json",
@@ -33,5 +36,41 @@ export default function AdminBody() {
 				setNextPageUrl(res.data.next_page_url);
 			});
 	};
-	return <div>Admin Body</div>;
+	const handleChange = (event, newValue) => {
+		setTabValue(newValue);
+	};
+	const allyProps = (index) => {
+		return {
+			id: `simple-tab-${index}`,
+			"aria-controls": `simple-tabpanel-${index}`,
+		};
+	};
+	return (
+		<>
+			<Box sx={{ width: "100%" }}>
+				<h3>Admin Body</h3>
+				<Tabs
+					value={tabValue}
+					onChange={handleChange}
+					textColor="primary"
+					indicatorColor="primary"
+					aria-label="primary tabs"
+					centered
+				>
+					<Tab label="Item One" {...allyProps(0)} />
+					<Tab label="Item Two" {...allyProps(1)} />
+					<Tab label="Item Three" {...allyProps(2)} />
+				</Tabs>
+			</Box>
+			<TabPanel value={tabValue} index={0}>
+				Item One
+			</TabPanel>
+			<TabPanel value={tabValue} index={1}>
+				Item Two
+			</TabPanel>
+			<TabPanel value={tabValue} index={2}>
+				Item Three
+			</TabPanel>
+		</>
+	);
 }

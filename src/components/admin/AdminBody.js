40a282d34828461b031fc5b295691d5a55eba8b3
Kristoffer Kyle Fernandez <kfernz2626@gmail.com>
2022-07-17 18:23:07 +0800

    Added backend stuff to AdminBody

--- src/components/admin/AdminBody.js
+++ src/components/admin/AdminBody.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AdminTable from "./AdminTable";
 import AdminCreateForm from "./AdminCreateForm";
 import AdminUpdateForm from "./AdminUpdateForm";
+import apiClient from "../../services/api";
 
 export default function AdminBody() {
+	const [users, setUsers] = useState([]);
+	const [nextPageUrl, setNextPageUrl] = useState("");
+	const token = JSON.parse(localStorage.getItem("user")).token;
+	const headers = {
+		accept: "application/json",
+		Authorization: "Bearer " + token,
+	};
+	useEffect(() => {
+		apiClient
+			.get("/api/users", {
+				headers: headers,
+			})
+			.then((res) => {
+				setUsers(res.data.data);
+				setNextPageUrl(res.data.next_page_url);
+			});
+	}, []);
+
+	const handleNextPage = () => {
+		apiClient
+			.get(nextPageUrl, {
+				headers: headers,
+			})
+			.then((res) => {
+				setUsers(res.data.data);
+				setNextPageUrl(res.data.next_page_url);
+			});
+	};
 	return <div>Admin Body</div>;
 }

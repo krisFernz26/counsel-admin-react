08675a433e922bb5b2ff28d2f19e9fec116b8874
Kristoffer Kyle Fernandez <kfernz2626@gmail.com>
2022-07-30 17:56:09 +0800

    Created InstitutionsBody.js

--- src/components/institution/InstitutionsBody.js
+++ src/components/institution/InstitutionsBody.js
@@ -1,5 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Box, Tabs, Tab, CircularProgress, TextField } from "@mui/material";
+import TabPanel from "../global/TabPanel";
+import InstitutionsTable from "./InstitutionsTable";
+import InstitutionsCreateForm from "./InstitutionsCreateForm";
 
 export default function InstitutionsBody() {
-	return <div>InstitutionsBody</div>;
+	const [tabValue, setTabValue] = useState(0);
+	const handleChange = (event, newValue) => {
+		setTabValue(newValue);
+	};
+	const allyProps = (index) => {
+		return {
+			id: `simple-tab-${index}`,
+			"aria-controls": `simple-tabpanel-${index}`,
+		};
+	};
+	return (
+		<>
+			<Box sx={{ width: "100%" }} mt={"64px"}>
+				<Tabs
+					value={tabValue}
+					onChange={handleChange}
+					textColor="primary"
+					indicatorColor="primary"
+					aria-label="primary tabs"
+					centered
+				>
+					<Tab label="All Institutions" {...allyProps(0)} />
+					<Tab label="Register new Institution" {...allyProps(1)} />
+				</Tabs>
+			</Box>
+			<TabPanel value={tabValue} index={0}>
+				<InstitutionsTable />
+			</TabPanel>
+
+			<TabPanel value={tabValue} index={1}>
+				<InstitutionsCreateForm />
+			</TabPanel>
+		</>
+	);
 }
